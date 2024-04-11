104b2062be0c2928c629e0ee767b113447222190
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2024-04-11 18:04:16 +0200

    trying to fix redirection

--- src/analytics.js
+++ src/analytics.js
@@ -10,13 +10,3 @@ export const initGA = () => {
 		gtag('config', 'G-5GDJMTB0LX');
 	});
 };
-
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-5GDJMTB0LX"></script>
-<script>
-	window.dataLayer = window.dataLayer || [];
-	function gtag(){dataLayer.push(arguments);}
-	gtag('js', new Date());
-
-	gtag('config', 'G-5GDJMTB0LX');
-</script>
