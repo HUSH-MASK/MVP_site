6d7fa368f701d89761a9900f77ae038950cdf1c9
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2024-04-11 15:23:12 +0200

    fixed footer for mask page

--- src/routes/mask/+page.svelte
+++ src/routes/mask/+page.svelte
@@ -315,30 +315,30 @@
 
 		</div>
 
-		<footer class="bg-hush-blue-800 rounded-lg shadow m-4 dark:bg-hush-blue-800">
-			<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-hush-white sm:text-center dark:text-hush-white">© 2024 <a href="{base}/home/"
-																																													class="hover:underline">HUSH!™</a>. All Rights Reserved.
-    </span>
-				<ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-hush-white dark:text-hush-white sm:mt-0">
-					<li>
-						<a href="{base}/home#about" class="hover:underline me-4 md:me-6">About</a>
-					</li>
-					<li>
-						<a href="{base}/home" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-					</li>
-					<li>
-						<a href="{base}/home" class="hover:underline me-4 md:me-6">Licensing</a>
-					</li>
-					<li>
-						<a href="{base}/home#about" class="hover:underline">Contact</a>
-					</li>
-				</ul>
-			</div>
-		</footer>
 	{/if}
 </MediaQuery>
 
+<footer class="bg-hush-blue-800 rounded-lg shadow m-4 dark:bg-hush-blue-800">
+	<div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm text-hush-white sm:text-center dark:text-hush-white">© 2024 <a href="{base}/home/"
+																																													class="hover:underline">HUSH!™</a>. All Rights Reserved.
+    </span>
+		<ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-hush-white dark:text-hush-white sm:mt-0">
+			<li>
+				<a href="{base}/home#about" class="hover:underline me-4 md:me-6">About</a>
+			</li>
+			<li>
+				<a href="{base}/home" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+			</li>
+			<li>
+				<a href="{base}/home" class="hover:underline me-4 md:me-6">Licensing</a>
+			</li>
+			<li>
+				<a href="{base}/home#about" class="hover:underline">Contact</a>
+			</li>
+		</ul>
+	</div>
+</footer>
 
 </body>
 <style lang="scss">
