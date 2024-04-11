1e20852084288463e04c0893cf6054705eea651d
Riccardo Gennaro <riccardo.gennaro@studenti.unitn.it>
2024-04-11 18:19:02 +0200

    fixing analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
   import "../app.css";
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	import Analytics from '$lib/analytics.svelte'
 	// This can be false if you're using a fallback (i.e. SPA mode)
 	export const prerender = true;
 </script>
--- src/routes/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-	import { initGA } from '../analytics.js';
-
-	// Initialize Google Analytics
-	initGA();
-</script>
